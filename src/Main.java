2c51835f8178cac8ffdcc67767f3d8f5733c138f
Ádria Aline Cortes <adria.aline@gmail.com>
2022-07-29 18:52:42 -0300

    Finalizado.

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,58 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Curso curso = new Curso();
-        curso.setTitulo("Curso Java");
-        curso.setDescricao("Aprendendo Java");
-        curso.setCargaHoraria(8);
+        Curso java = new Curso();
+        java.setTitulo("Curso Java");
+        java.setDescricao("Aprendendo Java");
+        java.setCargaHoraria(8);
 
-        Curso curso2 = new Curso();
-        curso2.setTitulo("Curso JS");
-        curso2.setDescricao("Aprendendo JS");
-        curso2.setCargaHoraria(4);
+        Curso javaScript = new Curso();
+        javaScript.setTitulo("Curso JS");
+        javaScript.setDescricao("Aprendendo JS");
+        javaScript.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria Java");
         mentoria.setDescricao("Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Treinando em Java");
+        bootcamp.getConteudos().add(java);
+        bootcamp.getConteudos().add(javaScript);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAdria = new Dev();
+        devAdria.setNome("Ádria Aline");
+        devAdria.inscreverBootcamp(bootcamp);
+        System.out.println("***** Aluno: ***** " +devAdria.getNome());
+        System.out.println("Contúdos inscritos: " + devAdria.getConteudosInscritos());
+        devAdria.progredir();
+        devAdria.progredir();
+        System.out.println("Contúdos concluidos: " + devAdria.getConteudosConcluidos());
+        System.out.println("Contúdos inscritos: " + devAdria.getConteudosInscritos());
+        System.out.println("XP: "+ devAdria.calcularXp());
+
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("***** Aluno: ***** " +devJoao.getNome());
+        System.out.println("Contúdos inscritos: " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("Contúdos concluidos: " + devAdria.getConteudosConcluidos());
+        System.out.println("Contúdos inscritos: " + devJoao.getConteudosInscritos());
+        System.out.println("XP: "+ devJoao.calcularXp());
+
+        /*System.out.println(curso);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
 
     }
 }
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,24 +1,13 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
-    private int cargaHoraria;
+public class Curso extends Conteudo {
 
-    public String getTitulo() {
-        return titulo;
-    }
+    private int cargaHoraria=0;
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public int getCargaHoraria() {
@@ -32,9 +21,11 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
+
+
 }
